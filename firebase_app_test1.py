0ca2fabc8c427e280ec1efc04cc9282c35206b42
PritamSapkal <pritam.s0752@gmail.com>
2026-06-20 22:57:22 +0530

    Refactor firebase_app_test1.py for clarity and structure

--- firebase_app_test1.py
+++ firebase_app_test1.py
@@ -3,11 +3,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from flask_cors import CORS
 
-
-
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
 
 # Firebase JSON Key
 firebase_config = {
